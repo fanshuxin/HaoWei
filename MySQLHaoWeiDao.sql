66c5e63fa2dc3dd8957de57962f75a63a0ecd1cc
fanshuxin <fshuxin@hotmail.com>
2016-05-20 22:27:48 -0600

    Add files via upload

---
+++ MySQLHaoWeiDao.sql
@@ -0,0 +1,65 @@
+
+CREATE DATABASE `HaoWeiDao`;
+USE  `HaoWeiDao`;
+/*login*/
+
+CREATE TABLE `roles` (
+  `Id` varchar(128) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+/* one factor login; two factors login*/
+CREATE TABLE `users` (
+  `Id` varchar(128) NOT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `EmailConfirmed` tinyint(1) NOT NULL,
+  `PasswordHash` longtext,
+  `SecurityStamp` longtext,
+  `PhoneNumber` longtext,
+  `PhoneNumberConfirmed` tinyint(1) NOT NULL,
+  `TwoFactorEnabled` tinyint(1) NOT NULL,
+  `LockoutEndDateUtc` datetime DEFAULT NULL,
+  `LockoutEnabled` tinyint(1) NOT NULL,
+  `AccessFailedCount` int(11) NOT NULL,
+  `UserName` varchar(256) NOT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `userclaims` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` varchar(128) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`),
+  KEY `UserId` (`UserId`),
+  CONSTRAINT `ApplicationUser_Claims` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE `userlogins` (
+  `LoginProvider` varchar(128) NOT NULL,
+  `ProviderKey` varchar(128) NOT NULL,
+  `UserId` varchar(128) NOT NULL,
+  PRIMARY KEY (`LoginProvider`,`ProviderKey`,`UserId`),
+  KEY `ApplicationUser_Logins` (`UserId`),
+  CONSTRAINT `ApplicationUser_Logins` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE `userroles` (
+  `UserId` varchar(128) NOT NULL,
+  `RoleId` varchar(128) NOT NULL,
+  PRIMARY KEY (`UserId`,`RoleId`),
+  KEY `IdentityRole_Users` (`RoleId`),
+  CONSTRAINT `ApplicationUser_Roles` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `IdentityRole_Users` FOREIGN KEY (`RoleId`) REFERENCES `roles` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ;
+/*driver license*/
+CREATE TABLE `images` (
+
+  `ImageId` varchar(128) NOT NULL,
+  `ImagePath` varchar(100),
+  `UserId` varchar(128) NOT NULL,
+  PRIMARY KEY (`ImageId`),
+   CONSTRAINT `ApplicationUser_Image` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
